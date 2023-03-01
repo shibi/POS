54f1f6c944381328096d9859db920b7b7fc79173
Shibin <shibinprk@gmail.com>
2023-03-01 10:19:25 +0530

    just commit

--- app/src/main/java/com/rpos/pos/MainActivity.java
+++ app/src/main/java/com/rpos/pos/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
